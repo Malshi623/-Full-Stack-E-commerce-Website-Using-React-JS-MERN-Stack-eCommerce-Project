e990ccdba6234ffeab048d288f401dae1c5b5225
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-02 14:40:51 +0530

    FS-ECW-FE-Create Sidebar menu

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../src/assets/assets'
 import {Link,NavLink} from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [visible, setVisible] = useState(false); //state variable
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} className='w-50' alt="" />
 
-        <ul className='hidden sm:flex gap-5 text-5m text-gray-700'>
+        {/*navigation bar menu list*/}
+        <ul className='hidden sm:flex gap-5 text-5m text-gray-700'> 
             <NavLink to='/' className='flex flex-col items-center gap-1'>
               <p>HOME</p>
               <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
@@ -25,7 +28,8 @@ const Navbar = () => {
               <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
         </ul>
-
+        
+        {/*search icon and profile icon*/}
         <div className='flex items-center gap-6'>
           <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
           <div className='group relative'>
@@ -38,10 +42,35 @@ const Navbar = () => {
               </div>
             </div>
           </div>
+          
+          {/*cart icon*/}
           <Link to='/cart' className='relative'>
             <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
             <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-squre rounded-full text-[8px]'>10</p>
           </Link>
+
+          {/*menu icon for small screens*/}
+          <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
+
+        </div>
+
+        {/*sidebar menu for small screen*/}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+
+          {/*sidebar menu closing icon */}
+          <div className='flex flex-col text-gray-600'>
+            <div onClick={()=>setVisible(false)} className='flex items-center cursor-pointer gap-4 p-3'>
+              <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+              <p>Back</p>
+            </div>
+
+            {/*Sidebar menu list */}
+            <NavLink to='/'>HOME</NavLink>
+            <NavLink to='/collection'>COLLECTION</NavLink>
+            <NavLink to='/about'>ABOUT</NavLink>
+            <NavLink to='/contact'>CONTACT</NavLink>
+          </div>
+
         </div>
 
     </div>
