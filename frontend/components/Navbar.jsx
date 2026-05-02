49b7b81909f95ecc676cd39af4fd91f280d4c458
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-02 11:30:46 +0530

    FS-ECW-FE-Add search /profile icon and dropdown menu

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -5,7 +5,8 @@ import {NavLink} from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-        <img src={assets.logo} className='w-56' alt="" />
+        <img src={assets.logo} className='w-50' alt="" />
+        
         <ul className='hidden sm:flex gap-5 text-5m text-gray-700'>
             <NavLink to='/' className='flex flex-col items-center gap-1'>
               <p>HOME</p>
@@ -24,6 +25,22 @@ const Navbar = () => {
               <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
             </NavLink>
         </ul>
+
+        <div className='flex items-center gap-6'>
+          <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+          <div className='group relative'>
+            <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+              <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                <p className='cursor-pointer hover:text-black'>My Profile</p>
+                <p className='cursor-pointer hover:text-black'>Orders</p>
+                <p className='cursor-pointer hover:text-black'>Logout</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
     </div>
   )
 }
