945c8dd3ef96e5444020cb9904cc8458ac8644be
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-02 11:03:59 +0530

    FS-ECW-FE-Create NavLinks

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -7,7 +7,22 @@ const Navbar = () => {
     <div className='flex items-center justify-between py-5 font-medium'>
         <img src={assets.logo} className='w-56' alt="" />
         <ul className='hidden sm:flex gap-5 text-5m text-gray-700'>
-            <NavLink className='flex flex-col items-center gap-1'></NavLink>
+            <NavLink to='/' className='flex flex-col items-center gap-1'>
+              <p>HOME</p>
+              <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+              <p>COLLECTION</p>
+              <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to='/about' className='flex flex-col items-center gap-1'>
+              <p>ABOUT</p>
+              <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+              <p>CONTACT</p>
+              <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
         </ul>
     </div>
   )
