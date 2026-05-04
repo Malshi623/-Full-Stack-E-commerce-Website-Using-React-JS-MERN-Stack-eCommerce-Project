1906d287a154b5dd63852d87e262bf951360640b
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-04 16:19:30 +0530

    FS-ECW-FE-Add texts for latest collection by usinf title component

--- frontend/components/LatestCollection.jsx
+++ frontend/components/LatestCollection.jsx
@@ -1,17 +1,20 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
 
 const LatestCollection = () => {
   
     /*Get products data from Context API */
     const {products} = useContext(ShopContext);
 
-    console.log(products);
+    /*check products were added */
+    /*console.log(products);*/
     
-
     return (
-    <div>
-      
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+      </div>
     </div>
   )
 }
--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import LatestCollection from '../components/LatestCollection'
+import Title from '../components/Title'
 
 const Home = () => {
   return (
