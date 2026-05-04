ad8a99c9b43b7194be97ef1ddac325a61df81339
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-04 13:37:37 +0530

    FS-ECW-FE-Create latest collection component

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import LatestCollection from '../components/LatestCollection'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <LatestCollection/>
     </div>
   )
 }
