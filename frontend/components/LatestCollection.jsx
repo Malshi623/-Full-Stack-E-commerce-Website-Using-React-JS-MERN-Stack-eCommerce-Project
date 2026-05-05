9c0799a965c71f1a7a305d47f109780294c15c64
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-05 12:16:46 +0530

    FS-ECW-FE-Get latest products data from products variable

--- frontend/components/LatestCollection.jsx
+++ frontend/components/LatestCollection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 
@@ -7,6 +7,12 @@ const LatestCollection = () => {
     /*Get products data from Context API */
     const {products} = useContext(ShopContext);
 
+    /*Get products from products variable to add 10 latest products in page */
+    const [latestProducts,setLatestProducts] = useState([]);
+    useEffect(()=>{
+      setLatestProducts(products.slice(0,10));
+    },[])
+
     /*check products were added */
     /*console.log(products);*/
     
@@ -14,6 +20,9 @@ const LatestCollection = () => {
     <div className='my-10'>
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+          Fresh arrivals made to keep your little ones stylish, happy, and comfortable every day.
+        </p>
       </div>
     </div>
   )
