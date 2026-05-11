5a2cd321f6250457dc6b9bb25ab23008971b3b75
Malshi623 <mhsm.dulakshi@gmail.com>
2026-05-11 13:16:11 +0530

    FS-ECW-FE-Create NewsLetter

---
+++ frontend/components/NewsLetter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NewsLetter = () => {
+  return (
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subscribe Now & Get 20% Off</p>
+      <p className='text-gray-400 mt-3'>Be the first to discover new arrivals, exclusive offers, and stylish collections for your little ones — delivered straight to your inbox.</p>
+    </div>
+  )
+}
+
+export default NewsLetter
